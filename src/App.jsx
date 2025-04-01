317d9bee3bfadec271cde06682baca47bae53ce2
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-04-01 19:56:24 +0200

    Update index.html title and add Google Fonts; check if everything connects fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import ToDoInput from "./components/ToDoInput"
+import ToDoList from "./components/ToDoList"
+import ToDoCard from "./components/ToDoCard"
 
 function App() {
 
   return (
-    <>
-      <div>{4 * 4}</div>
-
-      <div>aaaaaaaaaaaaaaaaaaaaaaaa </div>
-    </>
+    <main>
+      <ToDoInput/>
+      <ToDoList/>
+      <ToDoCard/>
+    </main>
   )
 }
 
